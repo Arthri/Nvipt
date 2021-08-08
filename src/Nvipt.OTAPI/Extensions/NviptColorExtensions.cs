12b020d01fe21a0dfe5b9a173fd3d14061705257
Arthri <41360489+Arthri@users.noreply.github.com>
2021-08-08 21:43:37 +0800

    feat(OTAPI): Add Color extensions

---
+++ src/Nvipt.OTAPI/Extensions/NviptColorExtensions.cs
@@ -0,0 +1,21 @@
+﻿using NviptColor = Nvipt.Core.Color;
+using XNAColor = Microsoft.Xna.Framework.Color;
+
+namespace Nvipt.OTAPI.Extensions
+{
+    /// <summary>
+    /// Provides helper methods for <see cref="Nvipt.Core.Color"/>.
+    /// </summary>
+    public static class NviptColorExtensions
+    {
+        /// <summary>
+        /// Returns a <see cref="Microsoft.Xna.Framework.Color"/> that represents the current color.
+        /// </summary>
+        /// <param name="color">The current color.</param>
+        /// <returns>A <see cref="Microsoft.Xna.Framework.Color"/> that represents the current color.</returns>
+        public static XNAColor ToXNAColor(this NviptColor color)
+        {
+            return new XNAColor((uint)color.PackedValue);
+        }
+    }
+}
---
+++ src/Nvipt.OTAPI/Extensions/XNAColorExtensions.cs
@@ -0,0 +1,21 @@
+﻿using NviptColor = Nvipt.Core.Color;
+using XNAColor = Microsoft.Xna.Framework.Color;
+
+namespace Nvipt.OTAPI.Extensions
+{
+    /// <summary>
+    /// Provides helper methods for <see cref="Microsoft.Xna.Framework.Color"/>.
+    /// </summary>
+    public static class XNAColorExtensions
+    {
+        /// <summary>
+        /// Returns a <see cref="Nvipt.Core.Color"/> that represents the current color.
+        /// </summary>
+        /// <param name="color">The current color.</param>
+        /// <returns>A <see cref="Nvipt.Core.Color"/> that represents the current color.</returns>
+        public static NviptColor ToNviptColor(this XNAColor color)
+        {
+            return new NviptColor((int)color.packedValue);
+        }
+    }
+}
