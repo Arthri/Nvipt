11b2e981e4384fae0715b04f46a07783fbd54a71
Arthri <41360489+Arthri@users.noreply.github.com>
2021-08-08 18:41:27 +0800

    feat: Expand inventory class

--- src/Nvipt.Core/Inventory.cs
+++ src/Nvipt.Core/Inventory.cs
@@ -6,8 +6,98 @@
     public class Inventory
     {
         /// <summary>
-        /// Represents this inventory's items.
+        /// Represents the size of <see cref="MainInventory"/>.
         /// </summary>
-        public BasicItem[] Items { get; set; }
+        public static readonly int InventorySize = 59;
+
+        /// <summary>
+        /// Represents the size of <see cref="Armor"/>.
+        /// </summary>
+        public static readonly int ArmorSize = 20;
+
+        /// <summary>
+        /// Represents the size of <see cref="Dye"/>.
+        /// </summary>
+        public static readonly int DyeSize = 10;
+
+        /// <summary>
+        /// Represents the size of <see cref="MiscEquips"/>.
+        /// </summary>
+        public static readonly int MiscEquipsSize = 5;
+
+        /// <summary>
+        /// Represents the size of <see cref="MiscDyes"/>.
+        /// </summary>
+        public static readonly int MiscDyesSize = 5;
+
+        /// <summary>
+        /// Represents the size of <see cref="PiggyBank"/>.
+        /// </summary>
+        public static readonly int PiggyBankSize = 40;
+
+        /// <summary>
+        /// Represents the size of <see cref="Safe"/>.
+        /// </summary>
+        public static readonly int SafeSize = 40;
+
+        /// <summary>
+        /// Represents the size of <see cref="DefendersForge"/>.
+        /// </summary>
+        public static readonly int DefendersForgeSize = 40;
+
+        /// <summary>
+        /// Represents the size of <see cref="VoidVault"/>.
+        /// </summary>
+        public static readonly int VoidVaultSize = 40;
+
+        /// <summary>
+        /// Represents the main inventory including the hot bar, coins, ammo and mouse item.
+        /// </summary>
+        public BasicItem[] MainInventory { get; }
+
+        /// <summary>
+        /// Represents the armor slots including the armor, accessories, and vanity.
+        /// </summary>
+        public BasicItem[] Armor { get; }
+
+        /// <summary>
+        /// Represents the dye slots.
+        /// </summary>
+        public BasicItem[] Dye { get; }
+
+        /// <summary>
+        /// Represents the miscellaneous equip slots, including the pet, light pet, mount, minecart, and hook.
+        /// </summary>
+        public BasicItem[] MiscEquips { get; }
+
+        /// <summary>
+        /// Represents the miscellaneous dye slots.
+        /// </summary>
+        public BasicItem[] MiscDyes { get; }
+
+        /// <summary>
+        /// Represents the piggy bank slots.
+        /// </summary>
+        public BasicItem[] PiggyBank { get; }
+
+        /// <summary>
+        /// Represents the safe(storage) slots.
+        /// </summary>
+        public BasicItem[] Safe { get; }
+
+        /// <summary>
+        /// Represents the trash item.
+        /// </summary>
+        public BasicItem TrashItem { get; }
+
+        /// <summary>
+        /// Represents the defender's forge slots.
+        /// </summary>
+        public BasicItem[] DefendersForge { get; }
+
+        /// <summary>
+        /// Represents the void vault slots.
+        /// </summary>
+        public BasicItem[] VoidVault { get; }
     }
 }
