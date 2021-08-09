36de8dbf8119812e34040951ed3d646cc7a7a87f
Arthri <41360489+Arthri@users.noreply.github.com>
2021-08-09 17:23:12 +0800

    build(build.sh): Produce cleaner output

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,14 @@
 mkdir bin/packages -p
 
+root=$PWD
+
 pushd . > /dev/null
 packages=$(readlink bin/packages -f)
 
 cd src/Nvipt.Core
-dotnet build -o $packages
+dotnet pack -o $packages
 
 popd > /dev/null
 
 cd src/Nvipt.OTAPI
-nuget pack -OutputDirectory $packages
+nuget pack Nvipt.OTAPI.csproj -OutputDirectory $packages -Build
