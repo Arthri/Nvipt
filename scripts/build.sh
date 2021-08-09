9fe74dfd6b2a8f08afc41a26f2ba4bb06787e912
Arthri <41360489+Arthri@users.noreply.github.com>
2021-08-09 17:23:39 +0800

    build: Add optional CLI option for configuration

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,15 @@
 mkdir bin/packages -p
 
+config=${1-"debug"}
 root=$PWD
 
 pushd . > /dev/null
 packages=$(readlink bin/packages -f)
 
 cd src/Nvipt.Core
-dotnet pack -o $packages
+dotnet pack -o $packages -c $config
 
 popd > /dev/null
 
 cd src/Nvipt.OTAPI
-nuget pack Nvipt.OTAPI.csproj -OutputDirectory $packages -Build
+nuget pack Nvipt.OTAPI.csproj -OutputDirectory $packages -Build -Properties Configuration=$config
